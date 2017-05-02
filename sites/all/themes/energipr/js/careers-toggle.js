5fc68406f24f77b99757b1ab1154096b8d78bdb6
Chris Morris <cmorris@ccistudios.com>
2017-05-02 12:32:57 -0400

    cleaned up the careers view

--- sites/all/themes/energipr/js/careers-toggle.js
+++ sites/all/themes/energipr/js/careers-toggle.js
@@ -3,8 +3,8 @@
 			$('.view-careers .info-button').click(function(e){
 				e.preventDefault();
 				var $row = $(this).closest('.views-row');
-				$row.find('.views-field-nothing').slideToggle();
-				$row.find('.views-field-nothing-1').toggleClass('open');
+				$row.find('.content').slideToggle();
+				$row.toggleClass('open');
 			});
 	});
 })(jQuery);
